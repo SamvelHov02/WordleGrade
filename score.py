1714575e30d258337dc330b127c2ced5b6379fca
Samvel Hovhannisyan <samnar2002@icloud.com>
2026-05-22 13:09:44 +0200

    feat: verbose option #4
    As mentioned in #4 it would be benefitial to have a verbose option that
    gives the best word at each guess according to expected information
    gain.

--- score.py
+++ score.py
@@ -9,7 +9,7 @@ def score_guess(valid_words, guess):
         guess (str) : Guess word to be evalauted
         
     Returns :
-        str : A grade from A - F
+        tuple(float, str, float) : A tuple with ratio of quality of the guess, best possible word and information_gain of hte best word
     """
     # If only one possible word left, return 1.0 if its the word otherwise 0
     if len(valid_words) == 1:
@@ -23,10 +23,10 @@ def score_guess(valid_words, guess):
     
     sorted_inf_gain = sorted(word_to_inf_gain.items(), key=lambda x : x[1], reverse=True)     
     
-    _, most_gain = sorted_inf_gain[0]
+    best_word, most_gain = sorted_inf_gain[0]
     guess_gain = word_to_inf_gain.get(guess, 0)
     
-    return guess_gain / most_gain
+    return (guess_gain / most_gain, best_word, most_gain)
 
 
 def score_game(game, word):
@@ -34,24 +34,29 @@ def score_game(game, word):
     
     Args :
         game (list) : The guesses player made in the game
+        word (str) : The answer for the wordle game
     
     Returns :
-        str : A grade from A - F
+        tuple(str, list(tuple(float, str, float))) : A grade from A - F and a list of information for each guess 
     """
     valid_words = read_valid_words()
     total_score = 0
+    # stores the information about each guess
+    optimal_game = []
     for i, guess in enumerate(game):
         # Treat the first guess as "free"
         if i != 0: 
-            total_score += score_guess(valid_words, guess)
+            score, b_word, b_gain = score_guess(valid_words, guess)
+            total_score += score 
+            optimal_game.append((score, b_word, b_gain))
 
         valid_words = remove_invalid_words(valid_words, word, guess)
         
     # Translate numeric score to a character grade
     avg_score = total_score / (len(word) - 1)
     
-    if avg_score >= 0.90: return "A"
-    if avg_score >= 0.75: return "B"
-    if avg_score >= 0.60: return "C"
-    if avg_score >= 0.45: return "D"
-    return "F"
+    if avg_score >= 0.90: return ("A", optimal_game)
+    if avg_score >= 0.75: return ("B", optimal_game)
+    if avg_score >= 0.60: return ("C", optimal_game)
+    if avg_score >= 0.45: return ("D", optimal_game)
+    return ("F", optimal_game)

4f97bc2200fc0b5819efa7e49c71223bf9f76f6f
Samvel Hovhannisyan <samnar2002@icloud.com>
2026-06-30 19:07:22 +0200

    fix : faulty logic
    In the MutationObserver callback the game was being constructed
    incorrectly, each letter input activated the callback and the game array
    got one letter words. For example when trying to input `CRANE`, the
    callback runs after C is inputed and the game array is C,,,,, since no
    other letter is yet handled. This is now fixed, however additional work
    needed to make sure that only actually submitted guesses are stored in
    the game, currently the extension doesn't check for that.

--- extension/grade.js
+++ extension/grade.js
@@ -1,3 +1,16 @@
+const waitFor = (selector) => {
+  return new Promise((resolve) => {
+    const exists = document.querySelector(selector);
+    if (exists) return resolve(exists);
+
+    const obs = new MutationObserver(() => {
+      const el = document.querySelector(selector);
+      if (el) {obs.disconnect(); resolve(el); }
+    });
+    obs.observe(document.body, {childList : true, subtree : true});
+  });
+}
+
 const config = {
     attributes : true,
     attributeFilter : ["data-state"],
@@ -8,21 +21,33 @@ const config = {
 const game = new Array(6).fill(null);
 
 const callback = async (mutations) => {
+    console.log("Get's to the callback");
     const index = game.findIndex(e => e === null);
     const rowElement = document.querySelector(`[aria-label="Row ${index + 1}"]`);   
+    console.log(`The selected row is ${rowElement}`);
 
     let guess = "";
     let pattern = true;
 
     for (const tile of rowElement.children){
-      guess += tile.innerText;
-      pattern = pattern && tile.getAttribute('data-state') === 'correct';
+      // Need to check that all children have a value since mutationObserver callback runs even if only one letter is entered
+      if (tile.innerText){ 
+        console.log(tile.innerText);
+        guess += tile.innerText;
+        pattern = pattern && tile.getAttribute('data-state') === 'correct';
+      } else {
+        return;
+      }
     }
 
+    // Append guess only after submittion
+
     game[index] = guess;
+    console.log(`The game thus far is ${game}`);
 
     // Send Grading request once 
     if (pattern){
+        console.log(`The game was won with that guess, ${guess} : good job`);
         const res = await fetch('http://localhost:8000/api/grade', {
           method : 'POST',
           headers : {'Content-Type' : 'application/json'},
@@ -41,7 +66,12 @@ const callback = async (mutations) => {
   }
 }
 
+console.log("Starts observing!!");
 const mutation = new MutationObserver(callback);
 
-mutation.observe()
+(async () => {
+  const targetNode = await waitFor('[class^=Board-module_board_]');
+  console.log(`Observing on the node ${targetNode}`);
+  mutation.observe(targetNode, config);
+})()
 

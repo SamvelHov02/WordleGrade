9f0e92834594c10916d83ebfd68d3cdadd42a950
Samvel Hovhannisyan <samnar2002@icloud.com>
2026-07-01 14:14:34 +0200

    fix : Handling game
    Fixed issues where the internal game wouldn't build correctly. Added so
    that only actually submitted words are added to the game. Also fixed
    a bug where incorrect HTMLDivElement was queried.

--- extension/grade.js
+++ extension/grade.js
@@ -22,28 +22,39 @@ const game = new Array(6).fill(null);
 
 const callback = async (mutations) => {
     console.log("Get's to the callback");
+    // console.log(`The game is ${game} : check 1`);
     const index = game.findIndex(e => e === null);
     const rowElement = document.querySelector(`[aria-label="Row ${index + 1}"]`);   
-    console.log(`The selected row is ${rowElement}`);
+    // console.log(`The selected row is ${rowElement}`);
+    const tiles = rowElement.querySelectorAll('[class^=Tile-module_tile_]');
 
     let guess = "";
     let pattern = true;
+    let submittedTags = ["correct", "absent", "present"];
+    let submitted = true;
 
-    for (const tile of rowElement.children){
+    for (const tile of tiles){
       // Need to check that all children have a value since mutationObserver callback runs even if only one letter is entered
+      console.log(`${tile.className}`);
       if (tile.innerText){ 
         console.log(tile.innerText);
         guess += tile.innerText;
-        pattern = pattern && tile.getAttribute('data-state') === 'correct';
+        let dataState = tile.getAttribute('data-state');
+        console.log(`The data-state is ${dataState}`);
+        pattern = pattern &&  dataState === 'correct';
+        submitted = submitted && submittedTags.includes(dataState);
       } else {
         return;
       }
     }
 
     // Append guess only after submittion
-
-    game[index] = guess;
-    console.log(`The game thus far is ${game}`);
+    if (submitted){
+      game[index] = guess;
+      console.log(`The game thus far is ${game}`);
+    } else {
+      return;
+    }
 
     // Send Grading request once 
     if (pattern){

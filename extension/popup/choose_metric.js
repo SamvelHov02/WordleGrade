8f5299e3cdb02633c9bd11987bb7189d5d8befe8
Samvel Hovhannisyan <samnar2002@icloud.com>
2026-07-11 22:45:56 +0200

    feat : Logged in Header

--- extension/popup/choose_metric.js
+++ extension/popup/choose_metric.js
@@ -1,3 +1,54 @@
+const checkAuth = async () => {
+    const token = localStorage.getItem('token');
+
+    if (!token){
+        console.log("No token found");
+        return null;
+    }
+
+    try{
+        const resp = await fetch('http://localhost:8000/api/me', {
+            headers : {'Authorization' : `Bearer ${token}`}
+        });
+        
+        // Token might be out of date
+        if (!resp.ok){
+            console.log("Token out of date");
+            localStorage.removeItem('token');
+            return null;
+        }
+
+        return await resp.json();
+    }
+    catch (err){
+        console.error("Auth check failed:", err);
+        return null;
+    }
+}
+
+const initPage = async () => {
+    const user = await checkAuth();
+
+    if (user) showLoggedInHeader(user);
+}
+
+
+const showLoggedInHeader = (user) => {
+    console.log("User is logged in");
+    // Remove default header children
+    const rootElement = document.querySelector('.header');
+    const elementsToRemove = document.querySelectorAll('.header-icon');
+    elementsToRemove.forEach((el) => rootElement.removeChild(el));
+
+    // Append new nodes
+    const profileElement = document.createElement('img');
+    profileElement.src = "assets/user.svg";
+    profileElement.alt = "Profile";
+    profileElement.width = "32";
+    profileElement.height = "32"
+    rootElement.appendChild(profileElement);
+}
+
 const changeMetric = (clicked) => {
     const prevMetric = sessionStorage.getItem('metric');
     if (clicked === prevMetric) return;
@@ -43,3 +94,5 @@ metricElements.forEach((el) => {
         e.stopPropagation();
     });
 })
+
+initPage();

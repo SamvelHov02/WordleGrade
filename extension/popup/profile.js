38bac9bdf53bff5970b1a0beaaef033e4bc650f7
Samvel Hovhannisyan <samnar2002@icloud.com>
2026-07-12 19:11:11 +0200

    feat : profile page started

---
+++ extension/popup/profile.js
@@ -0,0 +1,4 @@
+// User should be able to go back to the home page
+const homeElement = document.getElementById('home');
+homeElement.addEventListener("click", () => window.location.href = "choose_metric.html");
+

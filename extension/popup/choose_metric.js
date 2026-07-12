9d5d82c08b44bbbbd5b6bb189b5f0b6e167d5a52
Samvel Hovhannisyan <120418349+SamvelHov02@users.noreply.github.com>
2026-07-12 18:58:00 +0200

    Issue/20 (#21)
    * feat : Registration Popup
    
    Implemented the pages for registration and login, the scripts and
    styling as well.
    
    * fix : CORS origins
    
    * feat : Logged in Header
    
    * fix : CORS allowence
    
    * feat : Persistant session
    
    Implemented such that the popop intially checks if the user is already
    logged in by checking for valid token in storage.

--- extension/popup/choose_metric.js
+++ extension/popup/choose_metric.js
@@ -1,3 +1,57 @@
+const checkAuth = async () => {
+    const token = localStorage.getItem('token');
+
+    if (!token){
+        console.log("No token found");
+        return null;
+    }
+
+    try{
+        const resp = await fetch('http://localhost:8000/api/me', {
+            headers : {'Authorization' : `Bearer ${token}`}
+        });
+        
+        // Token might be out of date
+        if (!resp.ok){
+            console.log("Token out of date");
+            localStorage.removeItem('token');
+            return null;
+        }
+
+        return await resp.json();
+    }
+    catch (err){
+        console.error("Auth check failed:", err);
+        return null;
+    }
+}
+
+const initPage = async () => {
+    const user = await checkAuth();
+
+    if (user) showLoggedInHeader(user);
+}
+
+
+const showLoggedInHeader = (user) => {
+    console.log("User is logged in");
+    // Remove default header children
+    const rootElement = document.querySelector('.header');
+    const elementsToRemove = document.querySelectorAll('.header-icon');
+    elementsToRemove.forEach((el) => rootElement.removeChild(el));
+
+    // Append new nodes
+    const profileElement = document.createElement('img');
+    profileElement.src = "assets/user.svg";
+    profileElement.alt = "Profile";
+    profileElement.width = "32";
+    profileElement.height = "32"
+    rootElement.appendChild(profileElement);
+
+    // TODO : Add Event Listener to take profile page
+    profileElement.addEventListener("click", () => window.location.href = "profile.html");
+}
+
 const changeMetric = (clicked) => {
     const prevMetric = sessionStorage.getItem('metric');
     if (clicked === prevMetric) return;
@@ -21,8 +75,21 @@ const changeMetric = (clicked) => {
 }
 
 // Add eventListeners
-const elements = document.querySelectorAll('.metric');
-elements.forEach((el) => {
+const headerElements = document.querySelectorAll('.header-icon');
+headerElements.forEach((el) => {
+    const clickedElement = el.id;
+    el.addEventListener("click", () => {
+        // Go to Clicked page
+        window.location.href = `${clickedElement}.html`;
+    });
+    el.addEventListener("contextmenu", (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+    });
+});
+
+const metricElements = document.querySelectorAll('.metric');
+metricElements.forEach((el) => {
     const clickedMetric = el.id;
     el.addEventListener("click", () => changeMetric(clickedMetric));
     el.addEventListener("contextmenu", (e) => {
@@ -30,3 +97,5 @@ elements.forEach((el) => {
         e.stopPropagation();
     });
 })
+
+initPage();
